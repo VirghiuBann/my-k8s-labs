813ee7b90c02847bf14748852ccd3722f1f9a5c8
virghiu <virghiu@gmail.com>
2026-02-18 13:48:41 -0400

    update: sample express app

--- sample-apps/hello-from-hostname/index.mjs
+++ sample-apps/hello-from-hostname/index.mjs
@@ -5,7 +5,9 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  const helloMsg = `<h1>Hello from the ${os.hostname()} VERSION 1.0.0 </h1>`
+  const helloMsg = `<h1>Hello from the ${os.hostname()}</h1>
+  <h2>Version 2.0.0</h2> 
+  `
   console.log(helloMsg)
   res.send(helloMsg)
 })
